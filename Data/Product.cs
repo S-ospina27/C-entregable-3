6674ec1bb4101e29ec7933d76e03597c57419dce
Santiago Ospina <santiagoospinarojas27@gmail.com>
2022-10-29 06:19:57 -0500

    taller profesor juan

---
+++ Data/Product.cs
@@ -0,0 +1,9 @@
+namespace Entregable3.Data;
+
+public class Product
+{
+    public int? id {get; set; }
+    public string? Code {get; set; }
+    public string? Name {get; set; }
+    public double? Price {get; set; }
+}
